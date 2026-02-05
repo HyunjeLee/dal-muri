d63849f8a8b9d0774f8599d202118b0c8c63c350
HyunjeLee <cyeclamen07@gmail.com>
2026-02-05 12:49:52 +0900

    build: Downgrade SDK versions and update dependencies
    - Downgrade `compileSdk` and `targetSdk` from 36 to 35.
    - Update Java compatibility and Kotlin JVM target from 11 to 17.
    - Downgrade various library versions in `libs.versions.toml`, including Core Ktx, JUnit, Espresso, Lifecycle, Activity Compose, Hilt, and Room.
    - Update `composeBom` version to 2024.12.01.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.dalmuri.dalmuri"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dalmuri.dalmuri"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -31,14 +31,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
