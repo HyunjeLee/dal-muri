408db0b68bff8d5ec5d20340c127aa0a68e01cd0
HyunjeLee <cyeclamen07@gmail.com>
2026-02-10 10:41:34 +0900

    build: Add Hilt Navigation Compose dependency(1.2.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
